d2bac9de8d1c707be0fbf422b3c6a4ce48dccc28
Joshua A. Hightower <hightower@milyli.com>
2018-09-18 16:40:13 -0500

    chore:added GravityDemo project

---
+++ Gravity/GravityDemo/Program.Create.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Gravity.Test.Helpers;
+using Relativity.Test.Helpers;
+
+namespace GravityDemo
+{
+    public partial class Program
+    {
+        static int Create()
+        {
+	        throw new NotImplementedException("");
+        }
+    }
+}
---
+++ Gravity/GravityDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gravity.Test.Helpers;
+using Relativity.API;
+using Relativity.Test.Helpers;
+
+namespace GravityDemo
+{
+    public partial class Program
+    {
+	    private static TestHelper helper;
+
+        static void Main()
+        {
+	        Init();
+
+            Console.WriteLine("Create new instance of object");
+	        var newArtifactId = Create();
+            
+	        
+	        // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+	    private static void Init()
+	    {
+			helper = new TestHelper();
+		}
+    }
+}
